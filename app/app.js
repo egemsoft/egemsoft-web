8056d028715ba1aeb2436985bbee68b9069af0ab
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-04-16 19:05:07 +0300

    initial commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('egem-web',['ngRoute','ui.bootstrap', 'scrollLoad']);
+
+// Route configuration
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', { });
+}]);
---
+++ app/controller/pageCtrls.js
@@ -0,0 +1,11 @@
+var AboutCtrl = function($scope) {
+	$scope.name = 'about';
+};
+
+var ProductsCtrl = function($scope) {
+	$scope.name = 'products';
+};
+
+var ReferencesCtrl = function($scope) {
+	$scope.name = 'references';
+}
