f0e987c206b0366d0798cc055c5d5f2e7da0d64b
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-06 17:36:25 +0300

    yandex maps api fix

--- app/controller/homeCtrl.js
+++ app/controller/homeCtrl.js
@@ -3,7 +3,7 @@
  * Constructs dynamic objects on home page. Provides lazy loading of sections, etc.
  * @function
  */
-var HomeCtrl = function($scope, $location, $route, $config, $activePage, $anchorScroll, $timeout) {
+var HomeCtrl = function($scope, $location, $route, $config, $activePage, $anchorScroll, $timeout, $q) {
 
   // Main slides
   $scope.slides = $config.home[$scope.language.key].slides;
@@ -19,6 +19,21 @@ var HomeCtrl = function($scope, $location, $route, $config, $activePage, $anchor
   }
 
   $scope.blocksSlides = blocksSlides;
+
+  $scope.placeMap = function() {
+    if(ymapsApi==null) { // if yandex maps api not loaded give it 5 secs.
+      $timeout(function(){
+        var map = new ymapsApi.Map("ymaps-map-id_139936384058655207154", {center: [29.03792383862299, 41.02652803715182], zoom: 15, type: "yandex#map"});
+        map.controls.add("zoomControl");
+        map.geoObjects.add(new ymaps.Placemark([29.038095, 41.023798], {balloonContent: "Egemsoft", iconContent: "3"}, {preset: "twirl#redIcon"}));
+      },5000);
+    }
+    else {
+      var map = new ymapsApi.Map("ymaps-map-id_139936384058655207154", {center: [29.03792383862299, 41.02652803715182], zoom: 15, type: "yandex#map"});
+      map.controls.add("zoomControl");
+      map.geoObjects.add(new ymaps.Placemark([29.038095, 41.023798], {balloonContent: "Egemsoft", iconContent: "3"}, {preset: "twirl#redIcon"}));
+    }
+  };
 
   /**
    * Operations on route change.
