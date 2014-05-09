9da9f2307d5caca0f20f8afa460175e985f55dbe
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-09 14:57:32 +0300

    dist update

--- app/app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('egem-web',['ngRoute','ui.bootstrap', 'scrollLoad']);
-
-// Route configuration
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', { });
-}]);
