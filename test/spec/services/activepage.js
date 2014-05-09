895efbea9792cab1a4da0619f129cac8c1cc7759
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-09 11:14:02 +0300

    tests added for activePage service

--- test/spec/services/activepage.js
+++ test/spec/services/activepage.js
@@ -11,8 +11,38 @@ describe('Service: activePage', function () {
     activePage = _activePage_;
   }));
 
-  it("activePage should not to be null", function() {
-    expect(activePage).not.toBe(null);
+  it('must be object', function() {
+    expect(activePage).not.toBe(undefined);
+    expect(typeof(activePage)).toBe('object');
   });
+
+  it('must has get function', function() {
+    expect(activePage.get).not.toBe(undefined);
+    expect(typeof(activePage.get)).toBe('function');
+  });
+
+  it('must has set function', function() {
+    expect(activePage.set).not.toBe(null);
+    expect(typeof(activePage.set)).toBe('function');
+  });
+
+  it('must has registerObserverCallback function', function() {
+    expect(activePage.registerObserverCallback).not.toBe(undefined);
+    expect(typeof(activePage.registerObserverCallback)).toBe('function');
+  });
+
+  it('set function must call multiple callback functions', function() {
+    var activePageToBeSet = 'home';
+    var callback = function() {
+      expect(activePage.get()).toBe(activePageToBeSet);
+    };
+    var callback2 = function() {
+      expect(activePage.get()).toBe(activePageToBeSet);
+    };
+    activePage.registerObserverCallback(callback);
+    activePage.registerObserverCallback(callback2);
+    activePage.set(activePageToBeSet);
+  });
+
 
 });
