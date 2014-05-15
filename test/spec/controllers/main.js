6f6be5d8bfeda37a070eb3e28c87589844490ed2
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-15 10:26:37 +0300

    tests added for main controller

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -15,4 +15,69 @@ describe('Controller: MainCtrl', function () {
       $scope: scope
     });
   }));
+
+  it('must be defined', function() {
+    expect(!!MainCtrl).toBe(true);
+  });
+
+  it('must has a scope', function() {
+    expect(!!scope).toBe(true);
+  });
+
+  it('must has an accurate config model', function() {
+    // inject config provider
+    inject(function(config) {
+      expect(scope.config).toBe(config);
+    });
+  });
+
+  it('must has an accurate sections model contains home page sections', function() {
+    // inject config provider
+    inject(function(config) {
+      expect(scope.sections).toBe(config.sections);
+    });
+  });
+
+  it('must has a valid loadedSections model', function() {
+    expect(!!scope.loadedSections).toBe(true);
+    expect(scope.loadedSections.length > 0).toBe(true);
+  });
+
+  it('must has a valid year model', function() {
+    expect(scope.year).toBe(new Date().getFullYear());
+  });
+
+  it('must has an accurate activePage model', function() {
+    // inject config provider
+    inject(function(activePage) {
+      expect(scope.activePage).toBe(activePage.get());
+    });
+  });
+
+  it('must has an accurate languages model representing available languages', function() {
+    // inject language provider
+    inject(function(language) {
+      expect(scope.languages).toBe(language.availableLanguages);
+    });
+  });
+
+  it('must has a valid language model representing active language', function() {
+    expect(!!scope.language).toBe(true);
+    expect(typeof(scope.language)).toBe('object');
+  });
+
+  it('must has a working setLanguage function', function() {
+    expect(!!scope.setLanguage).toBe(true);
+    // inject language provider
+    scope.setLanguage(scope.languages[0]);
+    expect(scope.language).toBe(scope.languages[0]);
+  });
+
+  it('must has a working locale function', function() {
+    expect(!!scope.locale).toBe(true);
+    // inject language provider
+    inject(function(language) {
+      expect(scope.locale('home')).toBe(language.strings['home'][scope.language.key]);
+    });
+  });
 });
