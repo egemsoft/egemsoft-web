1b9b9e1862235066e74e6efe7af7c106b6a93070
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-15 15:47:16 +0300

    xml copying added

--- Gruntfile.js
+++ Gruntfile.js
@@ -301,7 +301,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,xml}',
             '.htaccess',
             'CNAME',
             '*.html',
