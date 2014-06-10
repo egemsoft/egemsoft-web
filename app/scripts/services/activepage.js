27a5e01c2abbfd9e9a76fb766f8f0d24802ae2b2
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-06-10 20:14:51 +0300

    property docs added for config

--- app/scripts/services/activepage.js
+++ app/scripts/services/activepage.js
@@ -1,21 +1,26 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @name egemsoftWebApp.factory:activePage
- * @description
- * Service factory to store active page between controllers
- * Initial value is 'home'
- * @author Ismail Demirbilek
- * @function
- */
 angular.module('egemsoftWebApp')
+  
+  /**
+   * @ngdoc service
+   * @name egemsoftWebApp.factory:activePage
+   * @description
+   * Service factory to store active page between controllers
+   * Initial value is 'home'
+   * @author Ismail Demirbilek
+   * @function
+   */
   .factory('activePage', function () {
     var activePage = 'home';
     var observerCallbacks = [];
     var activePageService = {};
     
     /**
+     * @ngdoc method
+     * @name set
+     * @memberOf egemsoftWebApp.factory:activePage
+     * ædescription
      * Set method for active page variable.
      * @param {string} page - Active page to define on service.
      * @function
